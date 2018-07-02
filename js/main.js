1a10e809620228e43f213b0824ceef0eae2aef83
yuanzhijian <zhijian.yuan@rokyinfo.com>
2018-07-02 18:39:28 +0800

    1. 整理目录结构 2. 整理网点列表代码结构 3. 实现网点列表新功能

--- js/main.js
+++ js/main.js
@@ -1,42 +1,55 @@
+/**
+ *
+ * @file 主入口JS文件
+ *
+ */
+
+/**
+ * api 请求地址
+ *
+ * @const
+ * @type {string}
+ */
+var API_BASE_URL = 'http://cjl3.rokyinfo.net:7200';
+
+/**
+ * 省、市、区插件自动选中
+ */
 $(function () {
 
-  'use strict';
+    var $distpicker = $('#distpicker');
 
-  var $distpicker = $('#distpicker');
+    $distpicker.distpicker({
+        province: '福建省',
+        city: '厦门市',
+        district: '思明区'
+    });
 
-  $distpicker.distpicker({
-    province: '福建省',
-    city: '厦门市',
-    district: '思明区'
-  });
+    $('#reset').click(function () {
+        $distpicker.distpicker('reset');
+    });
 
-  $('#reset').click(function () {
-    $distpicker.distpicker('reset');
-  });
+    $('#reset-deep').click(function () {
+        $distpicker.distpicker('reset', true);
+    });
 
-  $('#reset-deep').click(function () {
-    $distpicker.distpicker('reset', true);
-  });
+    $('#destroy').click(function () {
+        $distpicker.distpicker('destroy');
+    });
 
-  $('#destroy').click(function () {
-    $distpicker.distpicker('destroy');
-  });
+    $('#distpicker1').distpicker();
 
-  $('#distpicker1').distpicker();
+    $('#distpicker2').distpicker({
+        province: '---- 所在省 ----',
+        city: '---- 所在市 ----',
+        district: '---- 所在区 ----'
+    });
 
-  $('#distpicker2').distpicker({
-    province: '---- 所在省 ----',
-    city: '---- 所在市 ----',
-    district: '---- 所在区 ----'
-  });
+    $('#distpicker4').distpicker(
+    );
 
-
-
-  $('#distpicker4').distpicker(
-  );
-
-  $('#distpicker5').distpicker({
-    autoSelect: false
-  });
+    $('#distpicker5').distpicker({
+        autoSelect: false
+    });
 
 });
